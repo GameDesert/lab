ae9948fcadaf4e5d7031078f193057d958d240e1
GameDesert <56128805+GameDesert@users.noreply.github.com>
2023-04-30 01:58:59 +0100

    Finished Wiki Previewer

--- prototype/gamescreen/scripts/index.js
+++ prototype/gamescreen/scripts/index.js
@@ -294,17 +294,47 @@ function searchWiki(searchquery) {
         srsearch: searchquery,
         srprop: "titlesnippet",
         format: "json",
-        origin: location.origin
+        origin: "*" // I can't believe I just had to change this. I spent an hour trying to debug CORS.
     });
     
     fetch(`${search_url}?${params}`)
         .then(function(response){return response.json();})
         .then(function(response) {
             if (response.query.searchinfo.totalhits != 0){
+                document.getElementById("wikisearchresultstitle").innerHTML = "Results";
+                document.getElementById("wikisearchresultstext").innerHTML = "";
                 for (let resultcount = 0; resultcount < response.query.searchinfo.totalhits; resultcount++) {
-                    console.log(response.query.search[resultcount].title)
+                    var wikiresultentry = "<li style='cursor: pointer;' onclick='loadWikiPage(" + response.query.search[resultcount].pageid + ", `" + response.query.search[resultcount].title + "`);'>" + response.query.search[resultcount].title + "</li>"
+                    console.log(wikiresultentry)
+                    document.getElementById("wikisearchresultstext").innerHTML += wikiresultentry;
                 } 
+            } else {
+                document.getElementById("wikisearchresultstext").innerHTML = "<pre>No Results</pre>";
             }
+        })
+        .catch(function(error){console.log(error);});
+    console.log("yo")
+}
+
+const parser = new DOMParser();
+
+function loadWikiPage(pageidval, title) {
+    var params = new URLSearchParams({
+        action: "parse",
+        pageid: pageidval,
+        redirects: true,
+        prop: "text",
+        format: "json",
+        origin: "*" // I can't believe I just had to change this. I spent an hour trying to debug CORS.
+    });
+    
+    fetch(`${search_url}?${params}`)
+        .then(function(response){return response.json();})
+        .then(function(response) {
+            document.getElementById("wikisearchresultstitle").innerHTML = title;
+            var loadedWiki = parser.parseFromString(response.parse.text['*'], 'text/html');
+            var loadedText = loadedWiki.getElementsByTagName('p')[0].innerText;
+            document.getElementById("wikisearchresultstext").innerHTML = loadedText;
         })
         .catch(function(error){console.log(error);});
 }

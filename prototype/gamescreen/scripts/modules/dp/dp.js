a4b77bef206ac15123bbb67a27d4747fbda00dcc
GameDesert <56128805+GameDesert@users.noreply.github.com>
2023-05-07 19:07:08 +0100

    added dice roller

--- prototype/gamescreen/scripts/modules/dp/dp.js
+++ prototype/gamescreen/scripts/modules/dp/dp.js
@@ -15,6 +15,13 @@
     - Add or subtract from total depending on sign
 */
 
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function parsedice(dicestring) {
     var total = 0
     dicestring = dicestring.toLowerCase();
@@ -24,36 +31,68 @@ function parsedice(dicestring) {
         var aors = ""
         if (parsed[group] != "") {
             if (parsed[group].indexOf('d') > -1) {
+                console.log("DIE")
                 if ((parsed[group].charAt(0) == "+") || (parsed[group].charAt(0) == "-")) {
-                    aors = parsed[group].charAt(0)
-                    currentnum = ""
-                    for (let char = 1; char < parsed[group].length; char++) {
-                        currentnum += parsed[group].charAt(char)
-                    }
-                    currentnum = Number(currentnum)
-                    total = total + currentnum
-                } else {
-                    currentnum = ""
-                    for (let char = 0; char < parsed[group].length; char++) {
-                        currentnum += parsed[group].charAt(char)
-                    }
-                    currentnum = Number(currentnum)
-                    if (aors == "+") {
-                        total = total + currentnum
-                    } else if (aors == "-") {
-                        total = total - currentnum
-                    }
-                }
-            } else {
-                if ((parsed[group].charAt(0) == "+") || (parsed[group].charAt(0) == "-")) {
+                    console.log("ADD/SUBTRACT DIE")
                     aors = parsed[group].charAt(0);
+                    var currentcoefficient = ""
+                    var currentnum = ""
+                    var whered = 0
 
                     for (let char = 1; char < parsed[group].length; char++) {
                         if (parsed[group].charAt(char) != "d") {
-                            currentnum += parsed[group].charAt(char)
+                            currentcoefficient += parsed[group].charAt(char)
                         } else {
+                            whered = char;
                             break
                         }
+                    }
+
+                    for (let pchar = (whered + 1); pchar < parsed[group].length; pchar++) {
+                        currentnum += parsed[group].charAt(pchar);
+                    }
+
+                    currentcoefficient = Number(currentcoefficient)
+                    currentnum = Number(currentnum)
+                    if (aors == "+") {
+                        total = total + (currentcoefficient * getRandomInt(1, currentnum))
+                        console.log("ADD")
+                    } else if (aors == "-") {
+                        total = Number(total - (currentcoefficient * getRandomInt(1, currentnum)))
+                        console.log("SUBTRACT")
+                    }
+                } else {
+                    console.log("PURE DIE")
+                    var currentnum = "";
+                    var currentcoefficient = "";
+                    var whered = 0;
+                    for (let char = 0; char < parsed[group].length; char++) {
+                        if (parsed[group].charAt(char) != "d") {
+                            currentcoefficient += parsed[group].charAt(char)
+                            console.log(currentcoefficient)
+                        } else {
+                            whered = char;
+                            break
+                        }
+                    }
+
+                    for (let pchar = whered + 1; pchar < parsed[group].length; pchar++) {
+                        currentnum += parsed[group].charAt(pchar);
+                        console.log(currentnum)
+                    }
+
+                    currentcoefficient = Number(currentcoefficient);
+                    currentnum = Number(currentnum);
+                    total = total + (currentcoefficient * getRandomInt(1, currentnum));
+                }
+            } else {
+                console.log("MODIFIER")
+                if ((parsed[group].charAt(0) == "+") || (parsed[group].charAt(0) == "-")) {
+                    aors = parsed[group].charAt(0)
+                    console.log(aors)
+                    var currentnum = ""
+                    for (let char = 1; char < parsed[group].length; char++) {
+                        currentnum += parsed[group].charAt(char)
                     }
                     currentnum = Number(currentnum)
                     if (aors == "+") {
@@ -62,13 +101,9 @@ function parsedice(dicestring) {
                         total = total - currentnum
                     }
                 } else {
-                    var currentnum = ""
+                    currentnum = ""
                     for (let char = 0; char < parsed[group].length; char++) {
-                        if (parsed[group].charAt(char) != "d") {
-                            currentnum += parsed[group].charAt(char)
-                        } else {
-                            break
-                        }
+                        currentnum += parsed[group].charAt(char)
                     }
                     currentnum = Number(currentnum)
                     total = total + currentnum
@@ -78,5 +113,3 @@ function parsedice(dicestring) {
         }
     return total
 }
-
-/* Most of this is wrong. Add in the bits where it calculates the randomness from the thing after the "d", and take out the bits where it adds the coefficients directly to the total.*/
